fcdb77f6c34c173059f1ab99a6dd5bcf43c56b59
Ullas R <ullasr2004@gmail.com>
2024-12-25 17:26:10 +0530

    c

---
+++ module 29 LinkedList/Recursive_display.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+
+};
+void displayrec(Node* head){
+    if(head==NULL) return;
+    cout<<head->val<<" ";
+    displayrec(head->next);
+}
+
+int main(){
+Node* a=new Node(10);
+Node* b=new Node(20);
+Node* c=new Node(30);
+Node* d=new Node(40);
+
+a->next=b;
+b->next=c;
+c->next=d;
+    
+    displayrec(a);
+
+return 0;
+}
