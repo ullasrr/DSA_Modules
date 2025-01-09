24483c59e8d160ff26cec9d52b2fa872db53e658
Ullas R <ullasr2004@gmail.com>
2025-01-09 17:19:53 +0530

    c

--- module 32 Stacks/Array_as_stack.cpp
+++ module 32 Stacks/Array_as_stack.cpp
@@ -1,10 +1,40 @@
 #include<iostream>
 using namespace std;
 
+class Stack{
+public:
+int arr[5];
+int idx=-1;
+
+void push(int val){
+    idx++;
+    arr[idx]=val;
+}
+
+void pop(){
+    idx--;
+}
+
+int top(){
+    return arr[idx];
+}
+int size(){
+    return idx+1;
+}
+
+};
 
 int main(){
 
-    
+Stack st;
+st.push(10);
+st.push(20);
+st.push(30);
+
+cout<<st.size()<<endl;
+st.pop();
+cout<<st.top()<<endl;
+cout<<st.size();
 
 return 0;
 }
