4be93334c38b01b00ef2232856f7558fe5373ce2
Ullas R <ullasr2004@gmail.com>
2025-02-06 21:56:34 +0530

    heaps

--- module 42 HEAPS/Kth_smallest_element.cpp
+++ module 42 HEAPS/Kth_smallest_element.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 int main(){
     int arr[]={10,20,-4,5,18,24,1,-7,56};
-    int k=4;
+    int k=1;  //Kth element
     priority_queue<int> pq;
 
-    int n=sizeof(arr)/sizeof(arr[0]);
+    int n=sizeof(arr)/sizeof(arr[0]); //size
+
     for(int i=0;i<n;i++){
         pq.push(arr[i]);
-        if(pq.size()>4){
+        if(pq.size()>k){
             pq.pop();
         }
     }
+
+    cout<<pq.top();
 
 return 0;
 }
