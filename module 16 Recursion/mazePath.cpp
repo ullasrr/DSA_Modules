31214a4321df64bbd616964fb1c8dfce3614b960
ullasrr <ullasr2004@gmail.com>
2025-03-10 22:14:50 +0530

    c

--- module 16 Recursion/mazePath.cpp
+++ module 16 Recursion/mazePath.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 int maze(int sr,int sc,int er,int ec){
-    
+    if(sr>er || sc>ec) return 0;
+    if(sr==er && sc==ec) return 1;
+    int rightways=maze(sr+1,sc,er,ec);
+    int leftways=maze(sr,sc+1,er,ec);
+    int totalways=rightways+leftways;
+    return totalways;
 }
 
 int main(){
 
     cout<<maze(0,0,2,2);
+
 
 return 0;
 }
---
+++ module 19 Searching Algo/BinarySearch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int low=arr[0];
+    int high= arr[size];
+    int mid=low+high/2;
+    int target=4;
+    if(arr[mid]==target){
+        
+    }
+
+
+return 0;
+}
